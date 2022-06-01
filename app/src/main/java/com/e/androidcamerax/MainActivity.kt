d7e1f86a4db50899a2336f20dcd73052e98bd62a
Tília Nogueira <t.salomao.nogueira@avanade.com>
2022-06-01 19:04:00 -0300

    permissions

--- app/src/main/java/com/e/androidcamerax/MainActivity.kt
+++ app/src/main/java/com/e/androidcamerax/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.e.androidcamerax
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.e.androidcamerax.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,9 +14,26 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        if (allPermissionGranted()) {
+            Toast.makeText(this, "We have permission", Toast.LENGTH_SHORT).show()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                Constants.REQUIRED_PERMISSIONS,
+                Constants.REQUEST_CODE_PERMISSIONS
+            )
+        }
     }
+
+    //abre o dialog e pede permissao
+    private fun allPermissionGranted() =
+        Constants.REQUIRED_PERMISSIONS.all {
+            ContextCompat.checkSelfPermission(
+                baseContext, it
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+
 }
