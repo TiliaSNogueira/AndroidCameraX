a7ccd58a0d4a5cb199851b2a597529222ec3f6df
TiliaSNogueira <tiliasalomao@gmail.com>
2022-05-31 22:39:23 -0300

    configurações iniciais

---
+++ app/src/main/java/com/e/androidcamerax/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.e.androidcamerax
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.e.androidcamerax.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
